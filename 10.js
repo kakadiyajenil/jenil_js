59ad8e1a19e1b5fe3268518d4adcc94b78d628c8
jenil kakadiya <jenilkakadiya2602@gmail.com>
2023-12-16 09:36:21 +0530

    16-12-2023

---
+++ 10.js
@@ -0,0 +1,91 @@
+// let data = [11,22,33,44,55,66,77,88,99];
+
+// 13.>  Array Max ():-  ==> finde the array in Highet(Big)Number.
+// MaxNum = (a) =>{
+// return Math.max.apply(null,a);
+// };
+// console.log(MaxNum(data))
+
+// 14.>  Array Min ():-  ==> finde the array in lowest (Small) Number.
+// MinNum = (a) =>{
+// return Math.min.apply(null,a)
+// };
+// console.log(MinNum(data))
+
+// 15.> Array forEach ():- ==> 
+// let a = "";
+// data.forEach((val, index)=>{
+// a = a +" " + val;
+// })
+// console.log(a);
+
+// 16.> Array Map ():-   ==>  5 * 2 =10
+// let a = [];
+// a = data.map((val, index)=>{
+// return val - 5
+// })
+// console.log(a);
+
+// 17.> Array filter ():-
+// let a = [];
+//  a = data.filter((val, index)=>{ 
+// return val < 33;
+// })
+// console.log(a);
+
+// 18.> Array reduse ():-
+// let abc;
+// abc = data.reduceRight((a,val, index)=>{
+// return a + val;
+//  })
+// console.log(abc);
+
+// 20.> Array some():-
+//  let abc;
+//  abc = data.some((val, index)=>{
+//  return val < 25
+// })
+// console.log(abc);
+
+// 21.> Array every():-
+//  let abc;
+//  abc = data.every((val, index)=>{
+//  return val >= 25
+// })
+// console.log(abc);
+
+// 22.> Array find():-
+//  let abc;
+//  abc = data.find((val, index)=>{
+//  return val >= 25
+// })
+// console.log(abc);
+
+// 23.> Array findIndex():-
+// let abc;
+//  abc = data.findIndex((val, index)=>{
+//  return val >= 25
+// })
+// console.log(abc);
+
+// 24.> Array entries():-
+// let abc;
+//  abc = data.entries();
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+
+// 25.> Array includes():-
+// let abc;
+// abc = data.includes(11); // is true
+// console.log(abc);
+
+// 26.> Array copyWithin():-
+// console.log(data.copyWithin(0,4,7));
+// console.log(data.copyWithin(1,5));
